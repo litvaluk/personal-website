f1fc5026a2772676adfecb258562ce25da5a2b8f
Lukáš Litvan <lukas.litvan@gmail.com>
2021-11-25 23:34:50 +0100

    Add image alt text, remove react-icon types dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,9 @@ import { particlesOptions } from '../constants'
 const Home: NextPage = () => {
   return (
     <div>
-      <Particles options={particlesOptions} style={{}} />
+      <Particles options={particlesOptions} />
       <div className={styles.container}>
-        <Image className={styles.image} src={profilePic} width="250px" height="250px" layout="intrinsic" placeholder="blur" />
+        <Image className={styles.image} src={profilePic} alt="Photo of me standing on the Brooklyn Bridge" width="250px" height="250px" layout="intrinsic" placeholder="blur" />
         <h1>Lukáš Litvan</h1>
         <h2>Converting caffeine into code since 2017.</h2>
         <SocialIcons />
