c2a032650ced414e9ce0fdd07755d40027d9e186
Lukáš Litvan <lukas.litvan@gmail.com>
2021-12-02 21:07:14 +0100

    Add aria-label to social icons, increase their size

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,11 +4,11 @@ import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin, FaGit
 const SocialIcons = () => {
   return (
     <div className={styles.container}>
-      <a href="https://www.facebook.com/litvaluk/" ><FaFacebookSquare className={styles.icon} size="32" /></a>
-      <a href="https://www.instagram.com/litvanius/"><FaInstagramSquare className={styles.icon} size="32" /></a>
-      <a href="https://twitter.com/litvanius/"><FaTwitterSquare className={styles.icon} size="32" /></a>
-      <a href="https://www.linkedin.com/in/lukaslitvan/"><FaLinkedin className={styles.icon} size="32" /></a>
-      <a href="https://github.com/litvaluk/"><FaGithubSquare className={styles.icon} size="32" /></a>
+      <a href="https://www.facebook.com/litvaluk/" aria-label="Facebook"><FaFacebookSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.instagram.com/litvanius/" aria-label="Instagram"><FaInstagramSquare className={styles.icon} size="34" /></a>
+      <a href="https://twitter.com/litvanius/" aria-label="Twitter"><FaTwitterSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.linkedin.com/in/lukaslitvan/" aria-label="LinkedIn"><FaLinkedin className={styles.icon} size="34" /></a>
+      <a href="https://github.com/litvaluk/" aria-label="Github"><FaGithubSquare className={styles.icon} size="34" /></a>
     </div>
   )
 }
