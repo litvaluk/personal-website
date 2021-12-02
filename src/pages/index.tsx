dd67484864ca000757d7ca14bdbe8fce9d3e8323
Lukáš Litvan <lukas.litvan@gmail.com>
2021-12-02 23:48:00 +0100

    Remove tsparticles, change background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,10 @@ import Image from 'next/image'
 import SocialIcons from '../components/SocialIcons'
 import styles from '../styles/Home.module.css'
 import profilePic from '../../public/images/profile-pic.jpg'
-import Particles from "react-tsparticles";
-import { particlesOptions } from '../../constants'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Particles options={particlesOptions} />
       <div className={styles.container}>
         <Image className={styles.image} src={profilePic} alt="Photo of me standing on the Brooklyn Bridge" width="250px" height="250px" layout="intrinsic" placeholder="blur" />
         <h1>Lukáš Litvan</h1>
