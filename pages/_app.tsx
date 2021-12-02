2e9107f8dfc49ed98af902d74ed23e58558dd26d
Lukáš Litvan <lukas.litvan@gmail.com>
2021-12-02 15:06:10 +0100

    Improve SEO (meta, link tags)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,19 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Lukáš Litvan</title>
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Personal web page of a programmer named Lukáš Litvan, currently a college student at FIT CTU in Prague and a Java Developer at Broadridge Czech Republic, s.r.o." />
+        <meta name="keywords" content="Lukáš Litvan, litvaluk, programmer, developer, student" />
+        <meta name="author" content="Lukáš Litvan" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#262626" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta name="theme-color" content="#000000" />
+        <link rel="canonical" href="https://lukaslitvan.cz/" />
       </Head>
       <Component {...pageProps} />
     </>
