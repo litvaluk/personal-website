35783f27aa8e0b0ce64c659032e1817a60fb677d
Lukáš Litvan <lukas.litvan@gmail.com>
2021-12-02 14:39:21 +0100

    Add locale, use web font instead of local hosted font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Lukáš Litvan</title>
+        <link href="https://fonts.googleapis.com/css2?family=Courier+Prime" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
