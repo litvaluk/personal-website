8b223d46a683a5618e19f171f2962331115420a9
Lukáš Litvan <lukas.litvan@gmail.com>
2021-12-04 18:01:20 +0100

    Add noreferrer

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,11 +4,11 @@ import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin, FaGit
 const SocialIcons = () => {
   return (
     <div className={styles.container}>
-      <a href="https://www.facebook.com/litvaluk/" target="_blank" aria-label="Facebook"><FaFacebookSquare className={styles.icon} size="34" /></a>
-      <a href="https://www.instagram.com/litvanius/" target="_blank" aria-label="Instagram"><FaInstagramSquare className={styles.icon} size="34" /></a>
-      <a href="https://twitter.com/litvanius/" target="_blank" aria-label="Twitter"><FaTwitterSquare className={styles.icon} size="34" /></a>
-      <a href="https://www.linkedin.com/in/lukaslitvan/" target="_blank" aria-label="LinkedIn"><FaLinkedin className={styles.icon} size="34" /></a>
-      <a href="https://github.com/litvaluk/" target="_blank" aria-label="Github"><FaGithubSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.facebook.com/litvaluk/" target="_blank" rel="noreferrer" aria-label="Facebook"><FaFacebookSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.instagram.com/litvanius/" target="_blank" rel="noreferrer" aria-label="Instagram"><FaInstagramSquare className={styles.icon} size="34" /></a>
+      <a href="https://twitter.com/litvanius/" target="_blank" rel="noreferrer" aria-label="Twitter"><FaTwitterSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.linkedin.com/in/lukaslitvan/" target="_blank" rel="noreferrer" aria-label="LinkedIn"><FaLinkedin className={styles.icon} size="34" /></a>
+      <a href="https://github.com/litvaluk/" target="_blank" rel="noreferrer" aria-label="Github"><FaGithubSquare className={styles.icon} size="34" /></a>
     </div>
   )
 }
