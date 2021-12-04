7e1e9ae859bbe3713e3c891c51b3ec7a4c91a6d4
Lukáš Litvan <lukas.litvan@gmail.com>
2021-12-04 17:57:30 +0100

    Open social icon links on a new tab

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,13 +4,13 @@ import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin, FaGit
 const SocialIcons = () => {
   return (
     <div className={styles.container}>
-      <a href="https://www.facebook.com/litvaluk/" aria-label="Facebook"><FaFacebookSquare className={styles.icon} size="34" /></a>
-      <a href="https://www.instagram.com/litvanius/" aria-label="Instagram"><FaInstagramSquare className={styles.icon} size="34" /></a>
-      <a href="https://twitter.com/litvanius/" aria-label="Twitter"><FaTwitterSquare className={styles.icon} size="34" /></a>
-      <a href="https://www.linkedin.com/in/lukaslitvan/" aria-label="LinkedIn"><FaLinkedin className={styles.icon} size="34" /></a>
-      <a href="https://github.com/litvaluk/" aria-label="Github"><FaGithubSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.facebook.com/litvaluk/" target="_blank" aria-label="Facebook"><FaFacebookSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.instagram.com/litvanius/" target="_blank" aria-label="Instagram"><FaInstagramSquare className={styles.icon} size="34" /></a>
+      <a href="https://twitter.com/litvanius/" target="_blank" aria-label="Twitter"><FaTwitterSquare className={styles.icon} size="34" /></a>
+      <a href="https://www.linkedin.com/in/lukaslitvan/" target="_blank" aria-label="LinkedIn"><FaLinkedin className={styles.icon} size="34" /></a>
+      <a href="https://github.com/litvaluk/" target="_blank" aria-label="Github"><FaGithubSquare className={styles.icon} size="34" /></a>
     </div>
   )
 }
 
-export default SocialIcons
+export default SocialIcons;
